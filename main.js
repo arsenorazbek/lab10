fb6a62657b43e45ea3546e5cc450c4410b5f2a74
Arsen Orazbek <111432128+arsenorazbek@users.noreply.github.com>
2022-11-02 17:54:39 +0600

    Update main.js

--- main.js
+++ main.js
@@ -20,15 +20,18 @@ function main(lab1, lab2, lab3, lab4_5, lab6, lab8, quiz1){
     sum4_5=get_sum(lab4_5);
     sum6=get_sum(lab6);
     sum8=get_sum(lab8);
-    let total_score = quiz1+(sum1+sum2+sum3+sum4_5+sum6+sum8)*60/280;
+    sumtotal=sum1+sum2+sum3+sum4_5+sum6+sum8
+    let total_score = quiz1+(sumtotal)*60/280;
     const results={
         lab1:lab1,
         lab2:lab2,
         lab3:lab3,
         lab4_5:lab4_5,
         lab6:lab6,
+        lab8:lab8,
         quiz1:quiz1,
         sums: [sum1, sum2, sum3, sum4_5, sum6, sum8, quiz1], 
+        sumtotal:sumtotal,
         total_score: total_score,
     };
     return results
